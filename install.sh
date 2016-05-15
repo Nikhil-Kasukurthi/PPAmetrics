af0a8c17d9e3d85995acc76bce076f57cb988653
Nikhil Kasukurthi <nikhil.kasukurthi@gmail.com>
2016-05-15 18:00:47 +0530

    Shell script to install packages

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! bin/sh
+
+echo '************Installing pip************'
+sudo apt-get install python-pip -y
+
+echo '************Installing pstuil************'
+sudo pip install psutil
+
+echo '************Done Installing************'
