63aa4281c332e9aa32d229e5c2e0c4dfe7c4bea9
Nikhil Kasukurthi <nikhil.kasukurthi@gmail.com>
2017-03-02 11:02:39 +0530

    Update Memory.py

--- Memory.py
+++ Memory.py
@@ -85,7 +85,10 @@ def networkInfo():
     temp=psutil.net_io_counters(pernic=True)
 
     for nic in psutil.net_io_counters(pernic=True):
-        x={'NIC':nic,'Bytes sent': temp[nic].bytes_sent,'Bytes received':temp[nic].bytes_recv,'Packets Sent':temp[nic].packets_sent,
+        x={'NIC':nic,
+           'Bytes sent': temp[nic].bytes_sent,
+           'Bytes received':temp[nic].bytes_recv,
+           'Packets Sent':temp[nic].packets_sent,
            'Packet received':temp[nic].packets_recv}
         network_arr.append(x)
     data = {}
